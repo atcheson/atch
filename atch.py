e31111e8f55ea88f2b29f7e63a6dd8af990ef3ce
atcheson <m@atcheson.org>
2014-11-25 14:21:24 +0800

    substitutions and hooks mostly functional

--- atch.py
+++ atch.py
@@ -10,8 +10,12 @@ signal_str = re.compile(r"^.*\|----->(.*)$")
 atch_root = os.path.dirname(os.path.abspath(__file__))
 
 IGNORE_EXTENSIONS = ['.swp']
-VERBOSITY = 0
+VERBOSITY = 2
 ARG_DELIMITER = ':'
+SAFE_COMMANDS = ['recover']
+WHEN_SEPCHAR = '_'
+LIST_SEP= ','
+SCRIPTS_DIR = 'scripts'
 
 def scan_file(filepath):
     atchcmds = dict()
@@ -29,11 +33,11 @@ def vprint(string, level=1):
         print string
 
 
-def sep_commas(str_list):
-    return [x.strip() for x in str_list.split(',')]
+def sep_list(str_list):
+    return [x.strip() for x in str_list.split(LIST_SEP)]
    
 
-def build_index(dirpath, atch_type):
+def build_index(dirpath, atch_type, do_subs=True, atch_path=[]):
     index = dict()
     for relpath in os.listdir(dirpath):
         abspath = path.join(dirpath, relpath)
@@ -45,17 +49,18 @@ def build_index(dirpath, atch_type):
             atchcmds = scan_file(abspath)
             atchcmds['abspath'] = abspath
 
-            if 'invoke' in atchcmds:
-                atchcmds['invoke'] = run_inv_subs(atchcmds)
+            if 'invoke' in atchcmds and do_subs:
+                 atchcmds['invoke'] = run_subs(atchcmds, atch_path, 'index')
 
-            if atch_type in sep_commas(atchcmds['atch']):
-                for name in sep_commas(atchcmds['names']):
+            if atch_type in sep_list(atchcmds['atch']):
+                for name in sep_list(atchcmds['names']):
                     index[name] = atchcmds
 
         elif path.isdir(abspath):
-            subindex = build_index(abspath, atch_type)
+            subindex = \
+                    build_index(abspath, atch_type, atch_path.append(relpath))
             if subindex:
-                index[path.basename(abspath)] = build_index(abspath, atch_type)
+                index[path.basename(abspath)] = subindex
     return index
 
 
@@ -85,7 +90,7 @@ def build_hooktree(hookindex, when):
     hooktree = (dict(), [])
     for hook in hookindex:
         try:
-            hook_to = sep_commas(hookindex[hook][when])
+            hook_to = sep_list(hookindex[hook][when])
         except KeyError:
             continue
         for cmd in hook_to:
@@ -94,23 +99,33 @@ def build_hooktree(hookindex, when):
 
 
 
-def update_hooks(when, hooks_file=path.join(atch_root, 'atchhooks')):
-    vprint("updating atch hooks...", 1)
-    with open(hooks_file, 'wb') as f:
-        hook_index = build_index(path.join(atch_root, 'scripts') \
+def update_hooks(when, hooks_file=path.join(atch_root, 'hooks')):
+    vprint("updating " + when + " hooks...", 1)
+    with open(get_when_filename(hooks_file, when), 'wb') as f:
+        hook_index = build_index(path.join(atch_root, SCRIPTS_DIR) \
                 , atch_type = 'hook')
         hooks = build_hooktree(hook_index, when)
         pickle.dump(hooks, f)
         return hooks
 
 
-def update_index(index_file=path.join(atch_root, 'atchindex')):
-    vprint("updating atch index...", 1)
+def update_index(index_file=path.join(atch_root, 'index')):
+    vprint("updating index...", 1)
     with open(index_file, 'wb') as f:
-        index = build_index(path.join(atch_root, 'scripts',) \
+        index = build_index(path.join(atch_root, SCRIPTS_DIR) \
                 , atch_type = 'script')
         pickle.dump(index, f)
         return index
+
+
+def update_subs(when, subs_file=path.join(atch_root, 'substitutions')):
+    vprint("updating substitutions...", 1)
+    with open(get_when_filename(subs_file, when), 'wb') as f:
+        subs_index = build_index(path.join(atch_root, SCRIPTS_DIR) \
+                , atch_type = 'substitution', do_subs=False)
+        subs = build_hooktree(subs_index, when)
+        pickle.dump(subs, f)
+        return subs
 
 
 def get_source_path():
@@ -121,20 +136,45 @@ def get_source_path():
 
     return filepath
 
-def run_inv_subs(cmd):
-    inv_str = cmd['invoke']
-    inv_str = re.sub(r'\$atch_fcn "(.*)"', \
-            """python -c "import imp; """
-            """atch = imp.load_source('atch', '"""  
-               + get_source_path() + "'); " + r'\1' + '"', inv_str)   
-    inv_str = re.sub(r"\$this", cmd['abspath'], inv_str)
-    inv_str = re.sub(r" \./", ' ' + path.dirname(cmd['abspath']) + '/', inv_str)
-    inv_str = re.sub(r"\$atch_root", atch_root, inv_str)
-    vprint(inv_str, 2)
+
+def run_subs(inv_str, atch_path, when, params=None):
+    subtree = load_subs(when)
+     
+    for key in atch_path:
+        if subtree and key in subtree:
+            subtree = subtree[0][key]
+        else:
+            subtree = None
+    
+    if not subtree or not subtree[1]:
+        return inv_str
+
+    for sub in subtree[1]:
+        sub_cmd = sub['invoke']
+        if params:
+            for param in params:
+                sub_cmd +=  " " + param 
+        p = subprocess.popen(sub['invoke'], \
+            stdin = subprocess.PIPE, \
+            stdout = subprocess.PIPE, \
+            shell = True)
+        p.communicate(inv_str)
+        inv_str = p.communicate()
+
+    #inv_str = cmd['invoke']
+    #inv_str = re.sub(r'\$atch_fcn "(.*)"', \
+            #"""python -c "import imp; """
+            #"""atch = imp.load_source('atch', '"""  
+               #+ get_source_path() + "'); " + r'\1' + '"', inv_str)   
+    #inv_str = re.sub(r"\$this", cmd['abspath'], inv_str)
+    #inv_str = re.sub(r" \./", ' ' + path.dirname(cmd['abspath']) + '/', inv_str)
+    #inv_str = re.sub(r"\$atch_root", atch_root, inv_str)
+
+    vprint(inv_str, 3)
     return inv_str
 
 
-def load_index(index_file=path.join(atch_root, 'atchindex')):
+def load_index(index_file=path.join(atch_root, 'index')):
     try:
         with open(index_file, 'r') as f:
             return pickle.load(f)
@@ -142,12 +182,24 @@ def load_index(index_file=path.join(atch_root, 'atchindex')):
         return update_index(index_file)
 
 
-def load_hooks(when, hooks_file=path.join(atch_root, 'atchhooks')):
+def load_subs(when, subs_file=path.join(atch_root, 'substitutions')):
     try:
-        with open(hooks_file, 'r') as f:
+        with open(get_when_filename(subs_file, when), 'r') as f:
+            return pickle.load(f)
+    except (EOFError, IOError):   
+        return update_subs(when, subs_file)
+
+
+def load_hooks(when, hooks_file=path.join(atch_root, 'hooks')):
+    try:
+        with open(get_when_filename(hooks_file, when), 'r') as f:
             return pickle.load(f)
     except (EOFError, IOError):
         return update_hooks(when, hooks_file)
+
+
+def get_when_filename(hooks_file, when):
+    return hooks_file + WHEN_SEPCHAR + when
 
 
 def usage():
@@ -158,30 +210,33 @@ def cmd_not_found():
     print("deal with missing commands here")
 
 
-def runtime_substitution(inv_str, passed_args):
-    for f in  re.finditer(r'\$(\d+)', inv_str):
-        argument = passed_args[f.groups()[1:]]
-        inv_str = inv_str[:f.start()] + argument + inv_str[:f.end()]
-    return inv_str
+#def runtime_substitution(inv_str, passed_args):
+    #for f in  re.finditer(r'\$(\d+)', inv_str):
+        #argindex = f.groups()[0]
+        #if len(passed_args) >= argindex:
+            #argument = passed_args[argindex-1]
+        #else:
+            #argument = ''
+        #new_inv_str = inv_str[:f.start()] + argument + inv_str[f.end():]
+    #return new_inv_str
 
 
-def invoke(cmd, passed_args):
-    pdb.set_trace()
-    inv_str = runtime_substitution(cmd['invoke'], passed_args)
+def invoke(cmd, atch_path, passed_args, sub):
+    try:
+        inv_str = cmd['invoke']
+    except KeyError:
+        return False
+
+    if sub:
+      inv_str = run_subs(cmd['invoke'], atch_path, 'runtime', passed_args)
+
     vprint(inv_str, 2)
     subprocess.call(inv_str, shell=True)
 
 
 def run_hooks(hooktree, passed_args):
     for hook in hooktree[1]:
-        invoke(hook, passed_args)
-
-
-def tree_select(tree, key):
-    if key in tree:
-        return tree[key]
-    else:
-        return None
+        invoke(hook, None, passed_args, False)
 
 
 def main():
@@ -190,35 +245,46 @@ def main():
         usage()
         exit(1)
     
+    args = sys.argv[1:]
+    
+    safe = False
+    if ' '.join(args) in SAFE_COMMANDS:
+        safe = True
+        update_index()
+        update_hooks('before')
+        update_hooks('after')
+
     passed_args = []
     cmd = load_index()
     beforehooks = load_hooks('before')
     afterhooks = load_hooks('after')
-    args = sys.argv[1:]
-    try:
-        for arg_no, arg in enumerate(args):
-            if cmd == ARG_DELIMITER:
-                passed_args = args[arg_no + 1:]
-            if cmd and not passed_args:
-                cmd = tree_select(cmd, arg)
-            if not cmd:
-                if not passed_args:
-                    passed_args = args[arg_no:]
-            if afterhooks: 
-                afterhooks = tree_select(afterhooks[0], arg)
-            if beforehooks:
-                beforehooks = tree_select(beforehooks[0], arg)
-    except KeyError:
-        pass
+    atch_path = []
+
+    for arg_no, arg in enumerate(args):
+        if cmd and arg in cmd and not passed_args:
+            atch_path.append(args)
+            cmd = cmd[arg]
+        else:
+            passed_args = args[arg_no:]
+
+        if beforehooks and arg in beforehooks[0]:
+            beforehooks = beforehooks[0][arg]
+        else:
+            beforehooks = None
+
+        if afterhooks and arg in afterhooks[0]:
+            afterhooks = afterhooks[0][arg]
+        else:
+            afterhooks = None
 
     if beforehooks:
         run_hooks(beforehooks, passed_args)
     if cmd:
-        invoke(cmd, passed_args)
+        cmd_run = invoke(cmd, atch_path, passed_args, True)
     if afterhooks:
         run_hooks(afterhooks, passed_args)
 
-    if not beforehooks and not afterhooks and not cmd:
+    if not beforehooks and not afterhooks and not cmd_run and not safe:
         cmd_not_found()
 
     exit(0)
